176e0d2578443dd6e732776b8bffcd0103c2dbdc
YuxuanZhang <942326836zyx@gmail.com>
2025-10-18 02:15:08 +0800

    update utils

--- utils.py
+++ utils.py
@@ -5,76 +5,89 @@ import smplx
 import torch
 
 
-def load_data(path): 
+def load_data(path: str) -> dict: 
     with open(path, "r") as f: 
         data = json.load(f)
     return data
 
 
-def visualize(data, device): 
+def load_smplx(device: str) -> smplx.SMPLX: 
     model = smplx.create(
-        model_path="model",
-        model_type="smplx",
-        gender="neutral",
-        num_betas=10,
-        use_pca=False, 
-        flat_hand_mean=True,
-        create_global_orient=True,
-        create_body_pose=True,
-        create_left_hand_pose=True,
-        create_right_hand_pose=True,
-        create_betas=True,
-        create_expression=False,
-        create_jaw_pose=False,
-        create_leye_pose=False,
-        create_reye_pose=False
+        model_path             = "model",
+        model_type             = "smplx",
+        gender                 = "neutral",
+        num_betas              = 10,
+        use_pca                = False, 
+        flat_hand_mean         = True,
+        create_global_orient   = True,
+        create_body_pose       = True,
+        create_left_hand_pose  = True,
+        create_right_hand_pose = True,
+        create_betas           = True,
+        create_expression      = False,
+        create_jaw_pose        = False,
+        create_leye_pose       = False,
+        create_reye_pose       = False
     ).to(device)
     model.eval()
+    return model
 
-    transl          = data["transl"]
-    global_orient   = data["global_orient"]
-    body_pose       = data["body_pose"]
-    betas           = data["betas"]
-    left_hand_pose  = data["left_hand_pose"]
-    right_hand_pose = data["right_hand_pose"]
 
-    seq_len = body_pose.shape[0]
+def list_to_tensor(data, i, device): 
+    t   = torch.as_tensor(data["transl"]         [i]).to(device=device, dtype=torch.float32)
+    go  = torch.as_tensor(data["global_orient"]  [i]).to(device=device, dtype=torch.float32)
+    bp  = torch.as_tensor(data["body_pose"]      [i]).to(device=device, dtype=torch.float32)
+    b   = torch.as_tensor(data["betas"]          [i]).to(device=device, dtype=torch.float32)
+    lhp = torch.as_tensor(data["left_hand_pose"] [i]).to(device=device, dtype=torch.float32)
+    rhp = torch.as_tensor(data["right_hand_pose"][i]).to(device=device, dtype=torch.float32)
+    return t, go, bp, b, lhp, rhp
+
+
+def param_to_mesh(
+    model: smplx.SMPLX, 
+    transl: torch.Tensor, 
+    global_orient: torch.Tensor, 
+    body_pose: torch.Tensor, 
+    betas: torch.Tensor, 
+    left_hand_pose: torch.Tensor, 
+    right_hand_pose: torch.Tensor
+) -> o3d.geometry.TriangleMesh: 
+    with torch.no_grad():
+        output = model(
+            transl          = transl,
+            global_orient   = global_orient,
+            body_pose       = body_pose,
+            betas           = betas,
+            left_hand_pose  = left_hand_pose,
+            right_hand_pose = right_hand_pose,
+            return_verts    = True
+        )
+        vertices = output.vertices[0].detach().cpu().numpy().astype(np.float32)
+        faces    = model.faces.astype(np.int32)
+
+    mesh           = o3d.geometry.TriangleMesh()
+    mesh.vertices  = o3d.utility.Vector3dVector(vertices)
+    mesh.triangles = o3d.utility.Vector3iVector(faces)
+    mesh.compute_vertex_normals()
+    mesh.paint_uniform_color((0.7, 0.75, 0.9))
+    
+    return mesh
+
+
+def visualize(data, device): 
+    model = load_smplx(device)
+
+    seq_len = data["body_pose"].shape[0]
     for i in range(seq_len): 
-        t   = torch.as_tensor(transl[i]).to(device=device, dtype=torch.float32)
-        go  = torch.as_tensor(global_orient[i]).to(device=device, dtype=torch.float32)
-        bp  = torch.as_tensor(body_pose[i]).to(device=device, dtype=torch.float32)
-        b   = torch.as_tensor(betas[i]).to(device=device, dtype=torch.float32)
-        lhp = torch.as_tensor(left_hand_pose[i]).to(device=device, dtype=torch.float32)
-        rhp = torch.as_tensor(right_hand_pose[i]).to(device=device, dtype=torch.float32)
+        transl, global_orient, body_pose, betas, left_hand_pose, right_hand_pose = list_to_tensor(data, i, device)
 
-        with torch.no_grad():
-            output = model(
-                transl          = transl,
-                global_orient   = global_orient,
-                body_pose       = body_pose,
-                betas           = betas,
-                left_hand_pose  = left_hand_pose,
-                right_hand_pose = right_hand_pose,
-                return_verts    = True
-            )
-            vertices = output.vertices[0].detach().cpu().numpy().astype(np.float32)
-            faces = model.faces.astype(np.int32)
+        mesh = param_to_mesh(model, transl, global_orient, body_pose, betas, left_hand_pose, right_hand_pose)
 
-            mesh = o3d.geometry.TriangleMesh()
-            mesh.vertices = o3d.utility.Vector3dVector(vertices)
-            mesh.triangles = o3d.utility.Vector3iVector(faces)
-            mesh.compute_vertex_normals()
-            mesh.paint_uniform_color((0.7, 0.75, 0.9))
+        vis = o3d.visualization.Visualizer()
+        vis.create_window(window_name="SMPL-X", width=960, height=720, visible=True)
+        vis.add_geometry(mesh)
 
-            vis = o3d.visualization.Visualizer()
-            vis.create_window(window_name="SMPL-X", width=960, height=720, visible=True)
-            vis.add_geometry(mesh)
-
-            ctr = vis.get_view_control()
-            ctr.set_front(np.array(front))
-            ctr.set_lookat(np.array(lookat))
-            ctr.set_up(np.array(up))
-            ctr.set_zoom(1.0)
+        break
 
 
 def _test(): 
@@ -86,6 +99,7 @@ def _test():
         device = "cuda"
     else: 
         device = "cpu" 
+    visualize(data, device)
 
 
 if __name__ == "__main__": 
